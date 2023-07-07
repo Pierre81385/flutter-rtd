1aafc1d9856d506632148a169514fcc2392a0104
Peter Bishop <pjb.den@gmail.com>
2023-07-07 15:59:16 -0600

    styling update

--- rtd/lib/components/vehicle_drop.dart
+++ rtd/lib/components/vehicle_drop.dart
@@ -10,30 +10,76 @@ class VehicleSelection extends StatefulWidget {
 }
 
 class _VehicleSelectionState extends State<VehicleSelection> {
+  late TextStyle headSignStyle;
+
   List<DropdownMenuItem<String>> get dropdownItems {
     List<DropdownMenuItem<String>> menuItems = [
-      const DropdownMenuItem(value: "select", child: Text('Select a Line')),
-      const DropdownMenuItem(
-          value: "A", child: Text("A - Union Station to Denver Airport")),
-      const DropdownMenuItem(
-          value: "B", child: Text("B - Union Station to Westminster")),
-      const DropdownMenuItem(
+      DropdownMenuItem(
+          value: "select",
+          child: Text(
+            'SELECT A LINE...',
+            style: headSignStyle,
+          )),
+      DropdownMenuItem(
+          value: "A",
+          child: Text(
+            "A - Union Station to Denver Airport",
+            style: headSignStyle,
+          )),
+      DropdownMenuItem(
+          value: "B",
+          child: Text(
+            "B - Union Station to Westminster",
+            style: headSignStyle,
+          )),
+      DropdownMenuItem(
           value: "D",
-          child: Text("D - 18th & California to Littleton - Mineral")),
-      const DropdownMenuItem(
-          value: "E", child: Text("E - Union Station to RidgeGate Parkway")),
-      const DropdownMenuItem(
-          value: "G", child: Text("G - Union Station to Wheat Ridge Ward")),
-      const DropdownMenuItem(
-          value: "H", child: Text("H - 18th & California to Florida Station")),
-      const DropdownMenuItem(
-          value: "L", child: Text("L - 30th & Downing to 16th & Stout")),
-      const DropdownMenuItem(
-          value: "N", child: Text("N - Union Station to Eastlake")),
-      const DropdownMenuItem(
-          value: "R", child: Text("R - Peoria Station to RidgeGate Parkway")),
-      const DropdownMenuItem(
-          value: "W", child: Text("W - Union Station to JeffCo - Golden")),
+          child: Text(
+            "D - 18th & California to Littleton - Mineral",
+            style: headSignStyle,
+          )),
+      DropdownMenuItem(
+          value: "E",
+          child: Text(
+            "E - Union Station to RidgeGate Parkway",
+            style: headSignStyle,
+          )),
+      DropdownMenuItem(
+          value: "G",
+          child: Text(
+            "G - Union Station to Wheat Ridge Ward",
+            style: headSignStyle,
+          )),
+      DropdownMenuItem(
+          value: "H",
+          child: Text(
+            "H - 18th & California to Florida Station",
+            style: headSignStyle,
+          )),
+      DropdownMenuItem(
+          value: "L",
+          child: Text(
+            "L - 30th & Downing to 16th & Stout",
+            style: headSignStyle,
+          )),
+      DropdownMenuItem(
+          value: "N",
+          child: Text(
+            "N - Union Station to Eastlake",
+            style: headSignStyle,
+          )),
+      DropdownMenuItem(
+          value: "R",
+          child: Text(
+            "R - Peoria Station to RidgeGate Parkway",
+            style: headSignStyle,
+          )),
+      DropdownMenuItem(
+          value: "W",
+          child: Text(
+            "W - Union Station to JeffCo - Golden",
+            style: headSignStyle,
+          )),
     ];
     return menuItems;
   }
@@ -43,7 +89,7 @@ class _VehicleSelectionState extends State<VehicleSelection> {
   @override
   void initState() {
     selectedValue = "select";
-
+    headSignStyle = TextStyle(color: Colors.amber, fontWeight: FontWeight.bold);
     super.initState();
   }
 
@@ -53,7 +99,7 @@ class _VehicleSelectionState extends State<VehicleSelection> {
       padding: const EdgeInsets.all(8.0),
       child: Container(
         decoration: BoxDecoration(
-          color: Colors.white,
+          color: Colors.black,
           borderRadius: const BorderRadius.only(
               topLeft: Radius.circular(10),
               topRight: Radius.circular(10),
@@ -97,42 +143,24 @@ class _VehicleSelectionState extends State<VehicleSelection> {
                                                             : Colors.black,
                   ),
                   borderRadius: const BorderRadius.all(Radius.circular(10)))),
-          child: DropdownButton(
-            isExpanded: true,
-            value: selectedValue,
-            items: dropdownItems,
-            icon: Icon(
-              Icons.train,
-              color: selectedValue == "A"
-                  ? hexToArgbColor("57C1E9")
-                  : selectedValue == "B"
-                      ? hexToArgbColor("4E9D2D")
-                      : selectedValue == "D"
-                          ? hexToArgbColor("008348")
-                          : selectedValue == "E"
-                              ? hexToArgbColor("552683")
-                              : selectedValue == "G"
-                                  ? hexToArgbColor("F6B221")
-                                  : selectedValue == "H"
-                                      ? hexToArgbColor("0075BE")
-                                      : selectedValue == "L"
-                                          ? hexToArgbColor("FFCE00")
-                                          : selectedValue == "N"
-                                              ? hexToArgbColor("9F26B5")
-                                              : selectedValue == "R"
-                                                  ? hexToArgbColor("C4D600")
-                                                  : selectedValue == "W"
-                                                      ? hexToArgbColor("009DAA")
-                                                      : Colors.black,
+          child: DropdownButtonHideUnderline(
+            child: DropdownButton(
+              isExpanded: true,
+              value: selectedValue,
+              items: dropdownItems,
+              icon: Icon(
+                Icons.train,
+                color: Colors.amber,
+              ),
+              onChanged: (value) {
+                setState(() {
+                  selectedValue = value!;
+                });
+                widget.onChange(value!);
+                //print(value);
+                //print(selectedValue);
+              },
             ),
-            onChanged: (value) {
-              setState(() {
-                selectedValue = value!;
-              });
-              widget.onChange(value!);
-              //print(value);
-              //print(selectedValue);
-            },
           ),
         ),
       ),
